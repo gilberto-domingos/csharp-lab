50cbbcf1f2797a76ec8c6c22af9d266bc4ddd746
gilberto-domingos <domingoshot@hotmail.com>
2025-05-24 20:19:56 -0300

    feat: adiciona a solicitação e consulta via RabbitMQ

--- net_rabbitmq/Controllers/ApiEndPoints.cs
+++ net_rabbitmq/Controllers/ApiEndPoints.cs
@@ -1,10 +1,36 @@
+using RabbitMQ.Bus;
+using RabbitMQ.Relatorios;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+
 namespace RabbitMQ.Controllers;
 
-internal static class ApiEndPoints
+[ApiController]
+[Route("api/[Controller]")]
+internal static class ApiEndpoints
 {
-  public static void AddApiEndPoints(this WebApplication app)
-  {
-    app.MapGet("hello-world", () => new { saudacao = "hello world !" });
-  }
-    
+    public static void AddEndPointRabbit(this WebApplication app)
+    {
+        app.MapPost("solicitar-relatorio/{name}", async (string name, IBusPublish bus, CancellationToken ct = default) =>
+        {
+            var solicitacao = new SolicitacaoRelatorios()
+            {
+                Id = Guid.NewGuid(),
+                Nome = name,
+                Status = "Pendente",
+                ProcessedTime = null
+            };
+            
+            //Simulação: salvando no banco
+            Lista.Relatorios.Add(solicitacao);
+
+            var eventRequest = new RelatorioSolicitadoEvent(solicitacao.Id, solicitacao.Nome);
+
+            await bus.publishAsync(eventRequest, ct);
+
+            return Results.Ok(solicitacao);
+        });
+
+        app.MapGet("relatorios", () => Lista.Relatorios);
+    }
 }
