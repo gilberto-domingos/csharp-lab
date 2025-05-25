13ad7b67de8ffb8dbd57bcdfc844871f313497fe
gilberto-domingos <domingoshot@hotmail.com>
2025-05-24 21:12:29 -0300

    chore: atualiza configurações

--- net_rabbitmq/Program.cs
+++ net_rabbitmq/Program.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using DotNetEnv;
 using RabbitMQ.Controllers;
 using RabbitMQ.Extensions;
@@ -10,7 +9,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRabbitMqService();
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
@@ -20,7 +18,7 @@ builder.Services.AddSwaggerGen(c =>
         Description = "Documentação da RabbitMQ",
         Contact = new Microsoft.OpenApi.Models.OpenApiContact
         {
-            Name = "gilberto domingos jr",
+            Name = "Gilberto Domingos Jr",
             Email = "jrdomingosjr00@gmail.com",
         }
     });
@@ -41,46 +39,3 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.Run();
-=======
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
->>>>>>> ff8958a (feat: initial project)
