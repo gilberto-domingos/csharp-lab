50102f756b5b6e5a214d85f68449b2938ca703da
gilberto-domingos <domingoshot@hotmail.com>
2025-05-25 23:01:57 -0300

    chore(data) configura conexão e AppDbContext

--- asp.net-identity/Program.cs
+++ asp.net-identity/Program.cs
@@ -1,6 +1,24 @@
+using ApiIdentityEndpoint.Data;
+using ApiIdentityEndpoint.Models;
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+
+Env.Load();
+    
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = Env.GetString("CONNECTION_STRING");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+
+builder.Services.AddIdentityApiEndpoints<User>().AddEntityFrameworkStores<AppDbContext>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapIdentityApi<User>();
 
 app.Run();
