5ae81b8c2400671ae7673b577be6a0fb101e2c12
gilberto-domingos <domingoshot@hotmail.com>
2025-05-26 00:21:57 -0300

    chore(swagger): implem. configuração do swagger

--- asp.net-identity/Program.cs
+++ asp.net-identity/Program.cs
@@ -7,15 +7,21 @@ Env.Load();
     
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var connectionString = Env.GetString("CONNECTION_STRING");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddAuthentication();
 builder.Services.AddAuthorization();
-
 builder.Services.AddIdentityApiEndpoints<User>().AddEntityFrameworkStores<AppDbContext>();
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapSwagger();
 
 app.MapGet("/", () => "Hello World!");
 
