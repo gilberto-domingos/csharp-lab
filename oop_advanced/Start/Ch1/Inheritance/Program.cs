ab7625926e0ca4c073e687b7c4617a02ecea4edd
gilberto-domingos <domingoshot@hotmail.com>
2025-06-20 11:13:31 -0300

    feat: Herança
    feat: Modificadores de Acesso

--- oop_advanced/Start/Ch1/Inheritance/Program.cs
+++ oop_advanced/Start/Ch1/Inheritance/Program.cs
@@ -1,21 +1,25 @@
-﻿// Example file for Advanced C#: Object Oriented Programming by Joe Marini
-// Using inheritance to define a class hierarchy
+﻿
 
-// Instantiate some objects
 Circle c = new Circle(10);
 Rectangle r = new Rectangle(10,20);
 
-// TODO: Exercise the ToString() method
+Square s = new Square(10);
 
+Console.WriteLine($"{c}");
+Console.WriteLine($"{r}");
 
-// TODO: Use the "is" operator to test an object type
+Console.WriteLine($"{c is Shape2D}");
+Console.WriteLine($"{c is Rectangle}");
 
+Console.WriteLine(c.GetArea());
+Console.WriteLine(r.GetArea());
+Console.WriteLine(s.GetArea());
 
-// TODO: Call the GetArea() function on each one
+void PrintArea(Shape2D shape)
+{
+    Console.WriteLine(($"{shape.GetArea()}"));
+}
 
-
-// TODO: Print the area of each shape
-
-
-// TODO: All of the classes derive from Shape2D, so we can treat each one
-// as an instance of the base class. 
+PrintArea(c);
+PrintArea(r);
+PrintArea(s);
--- oop_advanced/Start/Ch1/Inheritance/shapes.cs
+++ oop_advanced/Start/Ch1/Inheritance/shapes.cs
@@ -1,30 +1,27 @@
-// Example file for Advanced C#: Object Oriented Programming by Joe Marini
-// Using inheritance to define a class hierarchy
-
-// Define a base class that represents the concept of a 2-dimensional shape
 class Shape2D {
     public Shape2D() {}
 
-    // TODO: Methods must be marked as "virtual" to allow overriding
+    public virtual float GetArea()
+    {
+        return 0.0f;
+    }
 
-
-    // TODO: All classes in .NET derive from Object, so we get some methods for free
-    // ToString() returns a string representation of the object
-    // GetType() returns the type of the object
+    public override string ToString() => $"This object is a '{GetType()}'";
 }
 
-// Derive a Circle class that inherits from the base Shape2D
 class Circle : Shape2D {
     public Circle(int r) {
         radius = r;
     }
 
-    // TODO: Override the GetArea() function for the Circle
+    public override float GetArea()
+    {
+        return (3.14f * (radius * radius));
+    }
 
     int radius;
 }
 
-// Derive a Rectangle class that inherits from the base Shape2D
 class Rectangle : Shape2D {
     public Rectangle(int w, int h) {
         width = w;
@@ -32,9 +29,16 @@ class Rectangle : Shape2D {
     }
 
     // TODO: Override the GetArea() function for the Rectangle
+    public override float GetArea()
+    {
+        return width * height;
+    }
 
     int width;
     int height; 
 }
 
-// TODO: Derive a Square class that inherits from the base Rectangle
+class Square : Rectangle
+{
+    public Square(int side) : base (side, side){}
+}
