19825d38407521ac2b30abcf732eb8ddd0404d81
gilberto-domingos <domingoshot@hotmail.com>
2025-06-21 21:17:29 -0300

    feat: Classes estáticas

--- oop_advanced/Start/Ch3/StaticClasses/Program.cs
+++ oop_advanced/Start/Ch3/StaticClasses/Program.cs
@@ -1,15 +1,8 @@
-﻿// Example file for Advanced C#: Object Oriented Programming by Joe Marini
-// Using static classes
+﻿Console.WriteLine($"There are {Converter.INCH_CM_CONVERT} cm per inch");
 
-// Access the static constant
-Console.WriteLine($"There are {Converter.INCH_CM_CONVERT} cm per inch");
-
-// Use the static class methods
 double result = Converter.InToCm(5.0);
 Console.WriteLine($"{result}");
 
 result = Converter.CmToIn(5.0);
 Console.WriteLine($"{result}");
 
-// static classes cannot be instantiated
-// Converter conv = new Converter(); // this will cause a compiler error
