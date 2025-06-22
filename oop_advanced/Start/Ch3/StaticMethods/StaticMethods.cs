185b4bcc4fdf3a41442eb3b0850039ee9472c7f5
gilberto-domingos <domingoshot@hotmail.com>
2025-06-21 21:02:16 -0300

    feat: Métodos estáticos

--- oop_advanced/Start/Ch3/StaticMethods/StaticMethods.cs
+++ oop_advanced/Start/Ch3/StaticMethods/StaticMethods.cs
@@ -2,7 +2,6 @@
 // Using static methods
 
 public class Temperature {
-    // constant values don't need to be defined as static, it is implied
     public const double ROOM_TEMP_LOWER_C = 20.0;
     public const double ROOM_TEMP_UPPER_C = 23.0;
 
@@ -10,18 +9,21 @@ public class Temperature {
         Temp = t;
     }
 
-    // TODO: static methods use the 'static' modifier in their declaration
-
-    // Regular property implementation
-    public double Temp { get; set; }
-
-    // TODO: You can overload methods and have one of them be static. This doesn't work with properties
-    public bool IsRoomTemp() {
-        return Temp >= ROOM_TEMP_LOWER_C && Temp <= ROOM_TEMP_UPPER_C;
+    public static double FtoC(double FTemp)
+    {
+        double CTemp = (FTemp - 32.0) * (5.0 / 9.0);
+        return CTemp;
     }
-
-    // This does not work - will cause a compile error
-    // public bool IsRoomTemp {
-    //     get => Temp >= ROOM_TEMP_LOWER_C && Temp <= ROOM_TEMP_UPPER_C;
-    // }
+    
+    public static double CtoF(double CTemp)
+    {
+        double FTemp = (CTemp * (9.0 / 5.0)) + 32.0;
+        return FTemp;
+    }
+    public double Temp { get; set; }
+    
+    public static bool IsRoomTemp(double temp) {
+        return temp >= ROOM_TEMP_LOWER_C && temp <= ROOM_TEMP_UPPER_C;
+    }
+    
 }
