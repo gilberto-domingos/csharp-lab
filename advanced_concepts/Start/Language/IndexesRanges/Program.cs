c0a43ae7fc954b30efe1dfa6069657f6bbef3dfa
gilberto-domingos <domingoshot@hotmail.com>
2025-06-23 16:22:42 -0300

    feat: Ìndices e intervalos (#2)

--- advanced_concepts/Start/Language/IndexesRanges/Program.cs
+++ advanced_concepts/Start/Language/IndexesRanges/Program.cs
@@ -1,21 +1,30 @@
-﻿// LinkedIn Learning Course exercise file for Advanced C# Programming by Joe Marini
-// Indexes and Ranges example
-
-// Define a sample array 
-string[] words = [
+﻿string[] words = [
     "The","quick","brown","fox","jumps",
     "over","the","lazy","dog"       
 ];
 
-// The index operator provides access to array elements
+Index idx = ^4;
+Console.WriteLine(words[idx]);
+
 Console.WriteLine(words[1]);
+Console.WriteLine(words[words.Length - 1]);
+Console.WriteLine(words[^1]);
 
-// The index-from-end operator indexes from the end of a sequence
-// (Note that the end is not zero-based, ^1 is the last item)
+Range rng = 3..^1;
 
+string[] WordRange;
 
-// The range operator (..) defines a range
+ WordRange = words[rng];
+Console.WriteLine($"{string.Join(",",WordRange)}");
 
+ WordRange = words[2..5];
+Console.WriteLine($"{string.Join(",",WordRange)}");
 
-// Indexes and ranges can be variables too
+ WordRange = words[2..];
+Console.WriteLine($"{string.Join(",",WordRange)}");
+
+ WordRange = words[..5];
+Console.WriteLine($"{string.Join(",",WordRange)}");
+
+ 
 
