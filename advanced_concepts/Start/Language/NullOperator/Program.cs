768a7c07039412b0f74fe9bd9e6b8d75461649f3
gilberto-domingos <domingoshot@hotmail.com>
2025-06-25 12:27:45 -0300

    feat: Operador de coalescência nulo (#4)

--- advanced_concepts/Start/Language/NullOperator/Program.cs
+++ advanced_concepts/Start/Language/NullOperator/Program.cs
@@ -1,24 +1,17 @@
-﻿// LinkedIn Learning Course exercise file for Advanced C# Programming by Joe Marini
-// Example file for the null coalescing operator
-
-// A common scenario is to test a value for null and assign one if it is
-void OldSchoolLogString(string? theString) {
+﻿void OldSchoolLogString(string? theString) {
     if (theString == null) {
-        Console.WriteLine("No string given!");
+        Console.WriteLine("Nenhuma string forncecida");
     }
     else {
         Console.WriteLine(theString);
     }
 }
 
-OldSchoolLogString("Test String");
+OldSchoolLogString("String de teste");
 OldSchoolLogString(null);
 
-// the ?? operator returns the left-hand value if not null, or the right one if it is null
+string? Str = null;
+//string? Str = "Alguma String";
 
-
-
-// It's also allowable to throw an exception as part of the right-hand expression
-
-
-// The ??= assigns a value if the left-hand value is null
+Str ??= "Se a string for nula vai mostrar essa string ou mensagem";
+Console.WriteLine(Str);
