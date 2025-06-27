863eeeb0d5e833e99420aa80982ff19f458ef2e3
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-06-27 15:52:44 -0300

    feat(pattern matching): basico (#4)

--- advanced_concepts/Start/PatternMatching/BasicPatterns/Program.cs
+++ advanced_concepts/Start/PatternMatching/BasicPatterns/Program.cs
@@ -1,43 +1,54 @@
-﻿// LinkedIn Learning Course exercise file for Advanced C# Programming by Joe Marini
-// C# Pattern Matching for type testing
-#pragma warning disable CS8321
+﻿#pragma warning disable CS8321
 
-// a very common use for pattern matching is to compare values, such as checking for null
-// The "is" expression is used for this to test the Constant pattern
-string? str = null;
+string? str = "no null";
 
+if (str is null)
+    ArgumentNullException.ThrowIfNull(str);
 
-// The "is" expression can also be used to extract a value if one is present
-// This is called the Declaration pattern
-void DashedLine(object o) {
-    // The old way of doing this is to try determine the type of the argument
-    int l = 0;
-    if (o.GetType() == typeof(int)) {
-        l = (int)o;
+Console.WriteLine($"{str}");
+
+void DashedLine(object o)
+{
+    int l1 = 0;
+
+    if (o.GetType() == typeof(int))
+    {
+        l1 = (int)o;
     }
-    string s;
-    if (o.GetType() == typeof(string)) {
-        s = (string)o;
-        if (!int.TryParse(s, out l)) {
-            l = 0;
+
+    string? s1 = null;
+    if (o.GetType() == typeof(string))
+    {
+        s1 = (string)o;
+        if (!int.TryParse(s1, out l1))
+        {
+            l1 = 0;
         }
     }
-    if (l > 0) {
-        string str = new string('-', l);
-        Console.WriteLine(str);
+
+    if (l1 > 0)
+    {
+        string str1 = new string('-', l1);
+        Console.WriteLine(str1);
     }
 
-    // The new way is to just use the declaration pattern
-
+    if (o is int l2 || (o is string s2 && int.TryParse(s2, out l2)))
+    {
+        string str2 = new string('-', l2);
+        Console.WriteLine(str2);
+    }
 }
 
-// Property pattern examines the properties of an object
-bool IsTheIdesOfMarch(DateTime date) {
-    // Test the month and day properties
-    return false;
+DashedLine(25);
+DashedLine("50");
+DashedLine(20.5);
+
+bool IsTheIdesOfMarch(DateTime date)
+{
+    return date is { Month: 3, Day: 14 or 15 };
 }
 
-// Console.WriteLine(IsTheIdesOfMarch(new DateTime(DateTime.Today.Year, 1, 13)));
-// Console.WriteLine(IsTheIdesOfMarch(new DateTime(DateTime.Today.Year, 3, 14)));
-// Console.WriteLine(IsTheIdesOfMarch(new DateTime(DateTime.Today.Year, 3, 15)));
-// Console.WriteLine(IsTheIdesOfMarch(new DateTime(DateTime.Today.Year, 3, 16)));
+Console.WriteLine(IsTheIdesOfMarch(new DateTime(DateTime.Today.Year, 1, 13)));
+Console.WriteLine(IsTheIdesOfMarch(new DateTime(DateTime.Today.Year, 3, 14)));
+Console.WriteLine(IsTheIdesOfMarch(new DateTime(DateTime.Today.Year, 3, 15)));
+Console.WriteLine(IsTheIdesOfMarch(new DateTime(DateTime.Today.Year, 3, 16)));
