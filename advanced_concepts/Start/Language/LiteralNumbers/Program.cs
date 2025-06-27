54d90ccb15e0f92c1e86a3343ab4ac5ef2b71639
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-06-27 00:04:58 -0300

    feat: Melhoria nos números literais (#2) (#7)

--- advanced_concepts/Start/Language/LiteralNumbers/Program.cs
+++ advanced_concepts/Start/Language/LiteralNumbers/Program.cs
@@ -1,10 +1,7 @@
-﻿// LinkedIn Learning Course exercise file for Advanced C# Programming by Joe Marini
-
-// Numbers can be made more readable using _ as a number separator
-int d = 123456;
-float f = 1234.5f;
-var x = 0xABCDEF;
-var b = 0b1101111010010010;
+﻿int d = 123_456;
+float f = 1_234.5f;
+var x = 0xAB_CD_EF;
+var b = 0b1101_1110_1001_0010;
 
 Console.WriteLine($"{d}");
 Console.WriteLine($"{f}");
