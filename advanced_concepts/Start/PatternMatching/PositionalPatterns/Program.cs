559d3db81d9c5fc6d5829b6b5864fd9bbf66dedd
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-06-30 16:28:38 -0300

    feat(Pattern Matching) Padrões posicionais (#6) (#6)

--- advanced_concepts/Start/PatternMatching/PositionalPatterns/Program.cs
+++ advanced_concepts/Start/PatternMatching/PositionalPatterns/Program.cs
@@ -1,30 +1,39 @@
-﻿// LinkedIn Learning Course exercise file for Advanced C# Programming by Joe Marini
-// Using Positional and Tuple Patterns in Pattern Matching
+﻿// decimal GetGroupTicketPriceDiscount(int groupSize, DateTime visitDate)
+//     => (groupSize, visitDate.DayOfWeek) switch
+// {
+//     
+//     (_,DayOfWeek.Saturday or DayOfWeek.Sunday) => 0.0m,
+//     (>= 5 and 10, DayOfWeek.Monday) => 0.20m,
+//     (>= 10, DayOfWeek.Monday) => 0.3m,
+//     (>= 5 and 10, _) => 0.12m,
+//     (>= 10,_) => 0.15m,
+//     (<= 0, _) => throw new ArgumentException("Group size must be positive"),
+//     _ => 0.0m,
+// };
+//
+// (int, DateTime)[] TestDiscountData = new[] {
+//     (4, new DateTime(2028, 9, 9)),
+//     (7, new DateTime(2028, 2, 7)),
+//     (20, new DateTime(2028, 4, 17)),
+//     (15, new DateTime(2028, 8, 8)),
+//     (9, new DateTime(2028, 8, 9)),
+// };
+//
+// foreach ((var size, var date) in TestDiscountData)
+// {
+//     decimal discount = GetGroupTicketPriceDiscount(size, date);
+//     Console.WriteLine($"The discount for a {size}-person group on {date:ddd,MMM d} is {discount}");
+// }
 
-decimal GetGroupTicketPriceDiscount(int groupSize, DateTime visitDate)
-    => (groupSize, visitDate.DayOfWeek) switch
+string Classify(Point point) => point switch
 {
-    // TODO: use the position of each value as an individual pattern expression
-
-    _ => 0.0m,
+    (>0, >0) => "Upper right",
+    (<0, >0) => "Upper left",
+    (>0, <0) => "Lower right",
+    (<0, <0) => "Lower left",
 };
 
-// Declare some test data to use with the example
-(int, DateTime)[] TestDiscountData = new[] {
-    (4, new DateTime(2028, 9, 9)),
-    (7, new DateTime(2028, 2, 7)),
-    (20, new DateTime(2028, 4, 17)),
-    (15, new DateTime(2028, 8, 8)),
-    (9, new DateTime(2028, 8, 9)),
-};
 
-// TODO: Iterate over each of the test data items and evaluate the discount
-
-
-// TODO: Use the implicit Deconstruct call to switch on the different values of a class
-
-
-// Declare some test data to use with the point example
 Point[] TestPointData = new[] {
     new Point(5, 8),
     new Point(-2, 7),
@@ -32,11 +41,10 @@ Point[] TestPointData = new[] {
     new Point(-2, -2),
 };
 
-// foreach (Point p in TestPointData) {
-//     Console.WriteLine($"Point is {Classify(p)}");
-// }
+foreach (Point p in TestPointData) {
+    Console.WriteLine($"Point is {Classify(p)}");
+}
 
-// Define a type that implements the Deconstruct method to return a tuple
 public readonly struct Point
 {
     public int X { get; }
