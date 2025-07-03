eceb3daef37640319afe53eb7a768116dde40a19
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-07-03 15:40:48 -0300

    feat(lambdas) Lambda como Delegate (#16) (#14)

--- advanced_concepts/Start/Lambdas/LambdaDelegates/Program.cs
+++ advanced_concepts/Start/Lambdas/LambdaDelegates/Program.cs
@@ -1,15 +1,11 @@
-﻿// LinkedIn Learning Course exercise file for Advanced C# Programming by Joe Marini
-// Example file for lambdas as delegates
-
-namespace LambdaDelegates
+﻿namespace LambdaDelegates
 {
-    // define a delegate 
     public delegate void myEventHandler(string value);
 
     class MyClass
     {
         private string? theVal = null;
-        public event myEventHandler? valueChanged; // no need to initialize to null, defaults to null
+        public event myEventHandler? valueChanged; 
 
         public string Val
         {
@@ -28,8 +24,11 @@ namespace LambdaDelegates
         {
             MyClass obj = new MyClass();
 
-            // TODO: Use a Lambda expression to define an event handler
-            // Note that this is a statement lambda, due to use of { }
+            obj.valueChanged += (x) =>
+            {
+                Console.WriteLine($"The value changed to {x}");
+            };
+            
 
             string? str;
             do
