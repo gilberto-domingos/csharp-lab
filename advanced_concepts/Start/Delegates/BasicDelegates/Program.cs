8eb7e54ca7acae6efe6efa3fb22131b6e16c2548
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-07-02 23:34:22 -0300

    feat(Delegados) Declaração e uso de delegados (#8) (#8)

--- advanced_concepts/Start/Delegates/BasicDelegates/Program.cs
+++ advanced_concepts/Start/Delegates/BasicDelegates/Program.cs
@@ -1,15 +1,10 @@
-﻿// LinkedIn Learning Course exercise file for Advanced C# Programming by Joe Marini
-// Example file for basic delegates
-
-namespace BasicDelegates
+﻿namespace BasicDelegates
 {
-    // TODO: declare the delegate type
 
+    public delegate string MyDelegate(int arg1, int arg2);
 
     class MyClass
     {
-        // Delegates can be bound to instance members as well as
-        // static class functions
         public string instanceMethod1(int arg1, int arg2)
         {
             return ((arg1 + arg2) * arg1).ToString();
@@ -18,16 +13,29 @@ namespace BasicDelegates
 
     class Program
     {
-        // TODO: Create functions to serve as delegate implementations
 
+        static string func1(int a, int b)
+        {
+            return (a + b).ToString();
+        }
+
+        static string func2(int a, int b)
+        {
+            return (a * b).ToString();
+        }
 
         static void Main(string[] args)
         {
-            // TODO: exercise each delegate function
 
+            MyDelegate f = func1;
+            Console.WriteLine($"The number is: {f(10,20)}");
 
-            // TODO: Use an instance function of a class as a delegate
+            f = func2;
+            Console.WriteLine($"The number is: {f(10,20)}");
 
+            MyClass mc = new MyClass();
+            f = mc.instanceMethod1;
+            Console.WriteLine($"The number is: {f(10,20)}");
 
         }
     }
