8f3e6e0b76d49dda471cc350352099788fc0021e
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-07-04 15:32:05 -0300

    feat(interfaces) Interfaces explícitas (#21) (#18)

--- advanced_concepts/Start/Interfaces/ExplicitInterfaces/Program.cs
+++ advanced_concepts/Start/Interfaces/ExplicitInterfaces/Program.cs
@@ -1,7 +1,4 @@
-﻿// LinkedIn Learning Course exercise file for Advanced C# Programming by Joe Marini
-// Example file for explicit interfaces
-
-namespace ExplicitInterfaces
+﻿    namespace ExplicitInterfaces
 {
     interface Intf1
     {
@@ -12,11 +9,18 @@ namespace ExplicitInterfaces
         void SomeMethod();
     }
 
-    // TODO: Implement two interfaces with conflicting methods
-    class InterfaceTest
+    class InterfaceTest: Intf1, Intf2
     {
-        // TODO: Implement Intf1 and Intf2 SomeMethod
 
+        void Intf1.SomeMethod()
+        {
+            Console.WriteLine("This in the Intf1 SomeMethod");
+        }
+        
+        void Intf2.SomeMethod()
+        {
+            Console.WriteLine("This in the Intf2 SomeMethod");
+        }
         public void SomeMethod() {
             Console.WriteLine("This is the class SomeMethod");
         }
@@ -28,8 +32,10 @@ namespace ExplicitInterfaces
             InterfaceTest testclass = new InterfaceTest();
             testclass.SomeMethod();
 
-            // TODO: Call SomeMethod in the two interfaces
-
+            Intf1 i1 = testclass as Intf1;
+            i1.SomeMethod();
+            Intf2 i2 = testclass as Intf2;
+            i2.SomeMethod();
         }
     }
 }
