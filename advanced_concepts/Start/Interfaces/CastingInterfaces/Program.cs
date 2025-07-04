0442713c147de964ab97c2f4a6a10d0c4615db47
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-07-04 14:27:05 -0300

    feat(interfaces) Interfaces e conversão de tipos (#18) (#16)

--- advanced_concepts/Start/Interfaces/CastingInterfaces/Program.cs
+++ advanced_concepts/Start/Interfaces/CastingInterfaces/Program.cs
@@ -37,9 +37,16 @@ namespace CastingInterfaces
         static void Main(string[] args) {
             Document d = new Document("Test Document");
 
-            // TODO: Use the 'is' operator
+            if (d is IStorable)
+            {
+                d.Save();
+            }
 
-            // TODO: Use the 'as' operator
+            IStorable istor = d as IStorable;
+            if (istor != null)
+            {
+                istor.Load();
+            }
 
         }
     }
