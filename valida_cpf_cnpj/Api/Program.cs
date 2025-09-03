e5aa4e555c0918ae1debc72fc869712ca4184706
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-09-03 16:16:15 -0300

    chore: config and files

--- valida_cpf_cnpj/Api/Program.cs
+++ valida_cpf_cnpj/Api/Program.cs
@@ -1,16 +1,41 @@
+using Application.Interfaces;
+using Application.Services;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "CpfCnpjValid API",
+        Version = "v1",
+        Description = "API simples para validação de CPF e CNPJ (DDD básico)",
+        Contact = new OpenApiContact
+        {
+            Name = "Gilberto Domingos",
+            Email = "gilberto1domingos1@gmail.com"
+        }
+    });
+});
+
 builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<IDocumentoService, DocumentoService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "CpfCnpjValid API V1");
+        c.RoutePrefix = string.Empty; 
+    });
     app.MapOpenApi();
 }
 
