0c37da44067af30ce808548ab1fd85e89389e1bd
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-09-16 18:08:10 -0300

    feat: Inherit of abstract class (#9)

---
+++ hackerrank/test_driven_development/AbstractClassInherit/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+abstract class Book
+{
+    protected string title;
+    protected string author;
+
+    public Book(string t, string a)
+    {
+        title = t;
+        author = a;
+    }
+
+    public abstract void display();
+}
+
+class MyBook : Book
+{
+    private int price;
+
+    public MyBook(string title, string author, int price) 
+        : base(title, author)
+    {
+        this.price = price;
+    }
+
+    public override void display()
+    {
+        Console.WriteLine("Title: " + title);
+        Console.WriteLine("Author: " + author);
+        Console.WriteLine("Price: " + price);
+    }
+}
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        string title = Console.ReadLine();
+        string author = Console.ReadLine();
+        int price = Int32.Parse(Console.ReadLine());
+
+        Book new_novel = new MyBook(title, author, price);
+        new_novel.display();
+    }
+}
