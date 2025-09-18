c11cc34d7eb35ce3cb9731f8798cb16eba65e3c0
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-09-17 21:02:11 -0300

    feat: Calc. factorial an int number (#6)

---
+++ hackerrank/RecursiveFactorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static int factorial(int n)
+    {
+        if (n <= 1)
+            return 1;
+
+        return n * factorial(n - 1);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um número inteiro para saber o fatorial e aperte enter.");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(factorial(n));
+    }
+}
