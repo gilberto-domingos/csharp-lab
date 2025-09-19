6cc5940280ed38eac9db3cbb349ef9edb7c27e27
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-09-19 15:40:48 -0300

    feat: Simple exception (#15)

---
+++ hackerrank/test_driven_development/SimpleException/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um número inteiro");
+        string Input = Console.ReadLine();
+
+        try
+        {
+            int value = int.Parse(Input);
+            Console.WriteLine(Input);
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine("Bad String", ex);
+        }
+    }
+}
