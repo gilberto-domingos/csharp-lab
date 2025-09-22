213a6f4e14fbe06797e02cff23c04ed3df8936f1
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-09-22 02:39:37 -0300

    refactor: atualiza, cria e deleta dtos (#127)

--- prints-control/Core/PrintsControl.Application/Dtos/Purchases/PurchaseDto.cs
+++ prints-control/Core/PrintsControl.Application/Dtos/Purchases/PurchaseDto.cs
@@ -2,6 +2,6 @@ using System;
 
 namespace PrintsControl.Application.Dtos.Purchases;
 
-public sealed record PurchaseDto(Guid Id, Guid StudentId, int Quantity, DateTimeOffset PurchaseDate);
+public sealed record PurchaseDto(Guid StudentId, int Quantity, DateTimeOffset PurchaseDate);
 
 
