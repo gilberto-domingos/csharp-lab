41a5c692fd8984b91894204de1db4b6e043f306f
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-09-29 14:55:45 -0300

    feat: Binary Search Trees  level-order traversal (#23)

---
+++ hackerrank/test_driven_development/BinarySearchTree/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Node {
+    public int data;
+    public Node left;
+    public Node right;
+    public Node(int d) { data = d; left = right = null; }
+}
+
+class Solution {
+    static Node Insert(Node root, int data) {
+        if (root == null) return new Node(data);
+        if (data <= root.data) root.left = Insert(root.left, data);
+        else root.right = Insert(root.right, data);
+        return root;
+    }
+
+    static int getHeight(Node root) {
+        if (root == null) return -1;                
+        int leftHeight  = getHeight(root.left);      
+        int rightHeight = getHeight(root.right);     
+        return 1 + Math.Max(leftHeight, rightHeight);
+    }
+
+    static void Main(string[] args) {
+        Console.WriteLine("Informe quantos números você vai informar para montar uma árvore binária ordenada e saber a altura da árvore");
+        int T = int.Parse(Console.ReadLine());
+        Node root = null;
+        while (T-- > 0) {
+            int data = int.Parse(Console.ReadLine());
+            root = Insert(root, data);
+        }
+        Console.WriteLine(getHeight(root));
+    }
+}
